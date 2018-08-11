a4a138943ebc8fada957b090a2586fa28d3b2594
Tarin <t.kasasagi@gmail.com>
2018-08-11 15:51:46 +0900

    I am doing my best

---
+++ getfilelist.py
@@ -0,0 +1,42 @@
+from tqdm import tqdm
+import numpy as np
+from skimage.data import imread
+import matplotlib.pyplot as plt
+
+#Get all file list
+from os import listdir
+
+directory = "binarization/"
+
+file_list = listdir(directory)
+edit_list = []
+threshold = 0.35
+
+for i in tqdm(range(len(file_list))):
+
+    image_file = directory + file_list[i]
+    im = imread(image_file)
+    
+    #Get shape
+    
+    shape = im.shape
+    #print(shape)
+    
+    
+    #print(im)
+    
+    #plt.figure(figsize=(30, 20))
+    #plt.imshow(im, cmap='gray')
+
+    
+    #Get peak histogram
+    
+    v_hist = (im < 25).mean(axis=0)
+    
+    #plt.figure(figsize=(30, 20))
+    #plt.plot(v_hist)
+    
+    index = np.where(v_hist == (np.max(v_hist)))
+    if (np.max(v_hist) < threshold) or (index[0][0] < 2000):
+        edit_list.append(file_list[i])
+    #print(type(index[0]))
